fce98b52828972432d2e19095969fa6f2979feef
Lucas <us3.lucas@gmail.com>
2025-05-29 10:43:29 -0300

    Update Individual.java

--- TaxPayer/src/entities/Individual.java
+++ TaxPayer/src/entities/Individual.java
@@ -1,17 +1,23 @@
 package entities;
 
+// classe Individual que herda de TaxPayer
 public class Individual extends TaxPayer {
 
+  // atribui ao TaxPayer Individual as despesas medicas
   private Double healthExpenditures;
 
+  // construtor padrao
   public Individual() {
     super();
   }
 
+  // construtor com argumentos
   public Individual(String name, Double anualIncome, Double healthExpenditures) {
     super(name, anualIncome);
     this.healthExpenditures = healthExpenditures;
   }
+
+  // getters e setters
 
   public Double getHealthExpenditures() {
     return healthExpenditures;
@@ -20,13 +26,15 @@ public class Individual extends TaxPayer {
   public void setHealthExpenditures(Double healthExpenditures) {
     this.healthExpenditures = healthExpenditures;
   }
-// Implementar o método tax() 
+
+  // Implementar o método tax()
   @Override
   public Double tax() {
     if (getAnualIncome() < 20000.0) {
       return getAnualIncome() * 0.15 - getHealthExpenditures() * 0.5;
+    } else {
+      return getAnualIncome() * 0.25 - getHealthExpenditures() * 0.5;
     }
-    else { return getAnualIncome() * 0.25 - getHealthExpenditures() * 0.5;}
-  } 
+  }
 
 }
