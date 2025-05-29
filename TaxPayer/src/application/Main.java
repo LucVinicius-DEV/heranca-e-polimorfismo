b38b37725b9d7a549af65c7de9b668a28a3b9a35
Lucas <us3.lucas@gmail.com>
2025-05-29 11:20:18 -0300

    Update Main.java

--- TaxPayer/src/application/Main.java
+++ TaxPayer/src/application/Main.java
@@ -17,32 +17,32 @@ public class Main {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    List<TaxPayer> list = new ArrayList<>(); // lista de tax payers
+    List<TaxPayer> list = new ArrayList<TaxPayer>(); // lista de tax payers
 
     System.out.print("Enter the number of tax payers: ");
     int n = sc.nextInt(); // numero de contribuintes informado pelo usuario
 
     for (int i=1; i<=n; i++)) { // loop para ler os dados de cada contribuinte
       
-      System.out.println("Tax payer #" + i + " data:");
+      System.out.println("Tax payer #" + i + " data:");// imprime o numero do contribuinte na ordem
       
       System.out.print("Individual or company (i/c)? ");
       char type = sc.next().charAt(0); // tipo de contribuinte informado pelo usuario
 
       System.out.print("Name: ");
-      sc.nextLine();
+      sc.nextLine(); // limpa o buffer do teclado
       String name = sc.nextLine(); // nome do contribuinte informado pelo usuario
 
       System.out.print("Anual income: ");
       Double anualIncome = sc.nextDouble(); // renda anual do contribuinte informado pelo usuario
 
       if (type == 'i') { // se o contribuinte for pessoa fisica
-        System.out.print("Health expenditures: ");
-        Double healthExpenditures = sc.nextDouble();
+        System.out.print("Health expenditures: "); 
+        Double healthExpenditures = sc.nextDouble(); // entao le as despesas medicas
         list.add(new Individual(name, anualIncome, healthExpenditures)); // adiciona o contribuinte na lista de contribuintes do tipo pessoa fisica
-      } else { // se o contribuinte for pessoa juridica
+      } else { // senao, se for contribuinte for pessoa juridica
         System.out.print("Number of employees: ");
-        Integer numberOfEmployees = sc.nextInt();
+        Integer numberOfEmployees = sc.nextInt(); // entao le o numero de funcionarios
         list.add(new Company(name, anualIncome, numberOfEmployees)); // adiciona o contribuinte na lista de contribuintes do tipo empresa
       }
     }
@@ -50,16 +50,17 @@ public class Main {
     // imprime os dados dos contribuintes e o valor da taxa de imposto de cada um
     System.out.println();
     System.out.println("TAXES PAID:");
-    for (TaxPayer tp : list) {
-      System.out.println(tp.getName() + ": $ " + String.format("%.2f", tp.tax()));
+    for (TaxPayer tp : list) { // loop para imprimir os dados de cada contribuinte
+      System.out.println(tp.getName() + ": $ " + String.format("%.2f", tp.tax())); // percorre a lista, e imprime o nome e a taxa de imposto de cada contribuinte
     }
+    
    // imprime o valor total das taxas de imposto
     System.out.println();
-    double sum = 0.0;
-    for (TaxPayer tp : list) {
-      sum += tp.tax();
+    double sum = 0.0; // cria uma variavel para armazenar a soma das taxas de imposto
+    for (TaxPayer tp : list) { // loop para somar as taxas de imposto de cada contribuinte
+      sum += tp.tax(); // percorre a lista, e soma as taxas de imposto de cada contribuinte
     }
-    System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
+    System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum)); // imprime o valor total das taxas de imposto
 
     sc.close();
   }
